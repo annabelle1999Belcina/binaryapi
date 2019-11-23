df3b3c45d9aa05365df937814398d740fd211bac
Jimboy Epilan <jepilan@student.passerellesnumeriques.org>
2019-11-23 11:02:22 +0800

    hjlds

--- models/User.js
+++ models/User.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = Schema({
-    fname: {type: String,required:true},
-    lname:{type: String,required:true},
+    firstName: {type: String,required:true},
+    lastName:{type: String,required:true},
     gender:{type: String,required:true},
     userName:{type: String,required:true},
     email:{type:String,required:true},
     password:{type: String,required:true},
     // isDeleted: {type: Boolean,default: false},
-    // signUpDate: {type: Date,default: Date.now()}
+    signUpDate: {type: Date,default: Date.now()}
 });
 
 var User = mongoose.model("User", userSchema);
