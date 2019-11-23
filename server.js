056714f6ed967f483495377238c4a4dda0089665
2ndyrGroupA <2ndyrGroupA@pn.local>
2019-11-23 11:50:09 +0800

    na save na jud

--- server.js
+++ server.js
@@ -77,64 +77,7 @@ app.post('/user/delete/:id', (req, res) => {
         return res.send({ message: 'Service is successfully deleted!', data })
     })
 })
-    // app.post('/user/signup', (req, res, next) => {
-    //     console.log("signup")
-    //     const { body } = req;
-    //     const { password } = body.password;
-    //     let { email } = body.email;
     
-    //     if (!email) {
-    //         return res.send({
-    //             success: false,
-    //             message: 'Error: Email cannot be blank.'
-    //         });
-    //     }
-    //     if (!password) {
-    //         return res.send({
-    //             success: false,
-    //             message: 'Error: Password cannot be blank.'
-    //         });
-    //     }
-    //     email = email.toLowerCase();
-    //     email = email.trim();
-    //     // Steps:
-    //     // 1. Verify email doesn't exist
-    //     // 2. Save
-    //     user.find({
-    //         email: email
-    //     }, (err, previousUsers) => {
-    //         if (err) {
-    //             return res.send({
-    //                 success: false,
-    //                 message: 'Error: Server error'
-    //             });
-    //         } else if (previousUsers.length > 0) {
-    //             return res.send({
-    //                 success: false,
-    //                 message: 'Error: Account already exist.'
-    //             });
-    //         }
-    //         //     // Save the new user
-    //         const newUser = new user({
-    //             firstName: req.body.firstName,
-    //             lastName: req.body.lastName,
-    //             gender: req.body.gender,
-    //             userName: req.body.userName,
-    //             email: req.body.email,
-    //             password: password,
-    //         });
-    //         newUser
-    //             .save()
-    //             .then(() => {
-    //                 console.log("New user added to database", newUser);
-    //                 res.json(newUser)
-    //             })
-    //             .catch(error => {
-    //                 console.log("Error: ", error);
-    //                 res.status(400).json({ message: error });
-    //             });
-    //         });
-    //     }); // end of sign up endpoint
 
     // app.post("/user/create", (req, res) => {
 //     const data = new user({
