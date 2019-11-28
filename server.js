585d3215b98ef3a79425e639f68176bdffc4ca88
annabelle1999Belcina <annabelle@student.passerellesnumeriques.org>
2019-11-28 10:32:07 +0800

    done pass encrypt

--- server.js
+++ server.js
@@ -9,13 +9,9 @@ const verify = require("./controller/verify");
 const user = require("./models/User");
 const imgRoutes = require('./controller/images')
 const multer = require('multer')
-<<<<<<< HEAD
 const path = require('path');
 const bcrypt = require('bcryptjs');
-=======
 
-const UserSession = require('./models/UserSession');
->>>>>>> 24065a0746cf7d3a1f3ef5997ea15d6a91f18689
 
 const PORT = process.env.PORT || 4000;
 //middleware
@@ -28,133 +24,73 @@ app.use(express.static('./images'));
 // Database
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server is running in PORT..," + PORT);
+    console.log("Server is running in PORT..," + PORT);
 });
 
 app.get("/", (req, res) => {
-  console.log("hello world!");
-  res.send("API");
+    console.log("hello world!");
+    res.send("API");
 });
-<<<<<<< HEAD
-// app.post("/login", (req, res) => {
-//     console.log(req.body);
-//     login.login(req.body, res);
-// });
-=======
-app.post("/login", (req, res) => {
-  console.log(req.body);
-  login.login(req.body, res);
+app.post("/login", (req, res) =>{
+	user.findOne({ userName: req.body.userName })
+		.then(user => {
+			console.log("User from login", user)
+			if (!user) res.sendStatus(204);
+			else {
+				bcrypt.compare(req.body.password, user.password)
+					.then(passwordMatch => passwordMatch ? res.sendStatus(200) : res.sendStatus(204))
+			}
+		})
 });
->>>>>>> 24065a0746cf7d3a1f3ef5997ea15d6a91f18689
 app.get("/verify/:token", (req, res) => {
-  verify.verify(req.params.token, res);
+    verify.verify(req.params.token, res);
 });
 
 app.post("/insert", (req, res) => {
-  insert.insert(req.body, res);
+    insert.insert(req.body, res);
 });
 
 app.get("/user/retrieve", (req, res) => {
-  user.find({}, (err, data) => {
-    if (err) {
-      return res.status(404).send("Error while getting list of services!");
-    }
-    return res.send({ data });
-  });
+    user.find({}, (err, data) => {
+        if (err) {
+            return res.status(404).send("Error while getting list of services!");
+        }
+        return res.send({ data });
+    });
 });
 
 // app.post("/user/create", (req, res) => {
-//   console.log("test");
-//   try {
-//     const data = new user(req.body);
-//     data.save((err, dbres) => {
-//       if (err) return res.status(404).send({ message: err.message });
-//       console.log(dbres);
-//       return res.send({ info: dbres, status: true });
-//     });
-//   } catch (err) {
-//     res.send({ message: err.message });
-//   }
-// });
-
-
-/*
- * Sign up
- */
-app.post('/user/signup', (req, res, next) => {
-  const { body } = req;
-  const {
-    password
-  } = body;
-  let {
-    email
-  } = body;
-
-  if (!email) {
-    return res.send({
-      success: false,
-      message: 'Error: Email cannot be blank.'
-    });
-  }
-  if (!password) {
-    return res.send({
-      success: false,
-      message: 'Error: Password cannot be blank.'
-    });
-  }
-  email = email.toLowerCase();
-  email = email.trim();
-  // Steps:
-  // 1. Verify email doesn't exist
-  // 2. Save
-  user.find({
-    email: email
-  }, (err, previousUsers) => {
-    if (err) {
-      return res.send({
-        success: false,
-        message: 'Error: Server error'
-      });
-    } else if (previousUsers.length > 0) {
-      return res.send({
-        success: false,
-        message: 'Error: Account already exist.'
-      });
-    }
-    // Save the new user
-    try {
-      const data = new user(req.body);
-      data.save((err, dbres) => {
-        if (err) return res.status(404).send({ message: err.message });
-        console.log(dbres);
-        return res.send({ info: dbres, status: true });
-      });
-    } catch (err) {
-      res.send({ message: err.message });
-    }
-  });
-})
-
-<<<<<<< HEAD
-  app.post("/login", (req, res)=> {
-    console.log('login test')
-    user.findOne({ userName: req.body.userName })
-      .then(user => {
-        console.log("User from login", user)
-        if (!user) res.sendStatus(204);
-        else {
-          bcrypt.compare(req.body.password, user.password)
-            .then(passwordMatch => passwordMatch ? res.sendStatus(200) : res.sendStatus(204))
-        }
+//     console.log("test");
+//     try {
+//       const data = new user(req.body);
+//       data.save((err, dbres) => {
+//         if (err) return res.status(404).send({ message: err.message });
+//         console.log(dbres);
+//         return res.send({ info: dbres, status: true });
+//       });
+//     } catch (err) {
+//       res.send({ message: err.message });
+//     }
+//   });
+  
+  app.post("/user/create", (req, res)=> {
+    let register = new user(req.body);
+    register.save()
+      .then(register => {
+        res.sendStatus(200);
+        console.log(register);
       })
+      .catch(err => {
+        console.log(err);
+        res.status(400).send("Failed to store to database");
+      });
   });
-  app.get("/getUser" ,(req, res) => {
-    console.log("getuser test")
-    user.findOne(req.body.userName, (err, user) => {
-      if (err) { res.send(err) }
-      else { res.send(user) };
-    })
-  });
+  // Username validation Router
+app.post("/user/validate", (req, res) => {
+	user.findOne({ user_name: req.body.user_name })
+		.then(user => user ? res.sendStatus(204) : res.sendStatus(200))
+});
+
 
   var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -162,133 +98,26 @@ app.post('/user/signup', (req, res, next) => {
     },
     filename: function (req, file, cb) {
       cb(null, file.fieldname + '-' + Date.now())
-=======
-app.post('/user/login', (req, res, next) => {
-  console.log("login test")
-  const { body } = req;
-  const {
-    password
-  } = body;
-  let {
-    email
-  } = body;
-  if (!email) {
-    return res.send({
-      success: false,
-      message: 'Error: Email cannot be blank.'
-    });
-  }
-  if (!password) {
-    return res.send({
-      success: false,
-      message: 'Error: Password cannot be blank.'
-    });
-  }
-  email = email.toLowerCase();
-  email = email.trim();
-  user.find({
-    email: email
-  }, (err, users) => {
-    if (err) {
-      console.log('err 2:', err);
-      return res.send({
-        success: false,
-        message: 'Error: server error'
-      });
-    }
-    if (users.length != 1) {
-      return res.send({
-        success: false,
-        message: 'Error: Invalid'
-      });
->>>>>>> 24065a0746cf7d3a1f3ef5997ea15d6a91f18689
-    }
-    // const user = users[0];
-    // if (!user.validPassword(password)) {
-    //   return res.send({
-    //     success: false,
-    //     message: 'Error: Invalid'
-    //   });
-    // }
-    // Otherwise correct user
-    const userSession = new UserSession();
-    userSession.userId = user._id;
-    userSession.save((err, doc) => {
-      if (err) {
-        console.log(err);
-        return res.send({
-          success: false,
-          message: 'Error: server error'
-        });
-      }
-      return res.send({
-        success: true,
-        message: 'Valid sign in',
-        token: doc._id
-      });
-    });
-  });
-});
-app.get('/user/verify', (req, res, next) => {
-  // Get the token
-  const { query } = req;
-  const { token } = query;
-  // ?token=test
-  // Verify the token is one of a kind and it's not deleted.
-  UserSession.find({
-    _id: token,
-    isDeleted: false
-  }, (err, sessions) => {
-    if (err) {
-      console.log(err);
-      return res.send({
-        success: false,
-        message: 'Error: Server error'
-      });
-    }
-    if (sessions.length != 1) {
-      return res.send({
-        success: false,
-        message: 'Error: Invalid'
-      });
-    } else {
-      // DO ACTION
-      return res.send({
-        success: true,
-        message: 'Good'
-      });
     }
   });
-});
+  
+  var upload = multer({ storage: storage });
 
-
-
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'images')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
-});
-
-var upload = multer({ storage: storage });
-
-app.post('/user/post', upload.single('photo'), (req, res, next) => {
-  console.log('hello')
-})
-
+  app.post('/user/post', upload.single('photo'), (req, res, next) => {
+    console.log('hello')
+  })
+  
 app.post("/user/update/:id", (req, res) => {
-  console.log(req.body);
-  user.findByIdAndUpdate(
-    req.params.id,//from database
-    req.body,//from the front end
-    { new: true },
-    (err, data) => {
-      if (err) return res.status(404).send({ error: err.message });
-      return res.send({ message: "Service is successfully updated", data });
-    }
-  );
+    console.log(req.body);
+    user.findByIdAndUpdate(
+        req.params.id,//from database
+        req.body,//from the front end
+        { new: true },
+        (err, data) => {
+            if (err) return res.status(404).send({ error: err.message });
+            return res.send({ message: "Service is successfully updated", data });
+        }
+    );
 });
 
 app.delete("/user/delete/:id", (request, response) => {
@@ -302,5 +131,19 @@ app.delete("/user/delete/:id", (request, response) => {
       response.status(400).json({ message: error });
     });
 });
+    
 
-
+    // app.post("/user/create", (req, res) => {
+//     const data = new user({
+//         firstName: req.body.firstName,
+//         lastName: req.body.lastName,
+//         gender: req.body.gender,
+//         userName: req.body.userName,
+//         email: req.body.email,
+//         password: req.body.password,
+//     });
+//     data.save(err => {
+//         if (err) return res.status(404).send({ message: err.message });
+//         return res.send({ data });
+//     });
+// });
