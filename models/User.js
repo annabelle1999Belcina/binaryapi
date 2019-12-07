e48ecd79e5a0b09e156044cd7587410e9621bc4e
daganasolro_sd2081@CEACCS.NET <daganasolro_sd2081@CEACCS.NET>
2019-12-07 09:52:04 +0800

    07-12-2019

--- models/User.js
+++ models/User.js
@@ -9,9 +9,15 @@ const userSchema = Schema({
     userName:{type: String,required:true},
     email:{type:String,required:true},
     password:{type: String,required:true},
+    signUpDate: {type: Date,default: Date.now()},
     // isDeleted: {type: Boolean,default: false},
-    signUpDate: {type: Date,default: Date.now()}
+    address:{type: String,required:true},
+    workPlace:{type: String,required:true},
+    education:{type: String,required:true},
+    relationShip:{type: String,required:true},
+    bio:{type: String,required:true}
 });
+
 // userSchema.methods.generateHash = function(password) {
 //     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 //   };
